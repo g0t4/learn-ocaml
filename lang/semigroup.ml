4292d3f2ae86b6673e37a83a513136182a212a27
Wes Higbee <wes.mcclure@gmail.com>
2024-08-06 00:41:12 -0600

    rebind printf

--- lang/semigroup.ml
+++ lang/semigroup.ml
@@ -24,9 +24,11 @@ let combine_all (type a) (module S : SEMIGROUP with type t = a) (list : a list) 
 let int_list = [1; 2; 3; 4; 5]
 let string_list = ["Hello"; " "; "World"; "!"]
 
+let printf = Printf.printf
+
 let () =
-  Printf.printf "Int result: %d\n" (combine_all (module IntAdditionSemigroup) int_list);
-  Printf.printf "String result: %s\n" (combine_all (module StringConcatSemigroup) string_list)
+  printf "Int result: %d\n" (combine_all (module IntAdditionSemigroup) int_list);
+  printf "String result: %s\n" (combine_all (module StringConcatSemigroup) string_list)
 
 (* Define a custom type *)
 type pair = { x : int; y : int }
@@ -42,14 +44,14 @@ let pair_list = [{ x = 1; y = 2 }; { x = 3; y = 4 }; { x = 5; y = 6 }]
 
 let () =
   let result = combine_all (module PairSemigroup) pair_list in
-  Printf.printf "Pair result: { x = %d; y = %d }\n" result.x result.y
+  printf "Pair result: { x = %d; y = %d }\n" result.x result.y
 
 let dup (type a) (items: a list) : (a list) =
   (* List.append items items *)
   items @ items
 
 let dump (items) =
-  Printf.printf "[%s]\n" (String.concat "; " (List.map string_of_int items))
+  printf "[%s]\n" (String.concat "; " (List.map string_of_int items))
 
 
 let () =
